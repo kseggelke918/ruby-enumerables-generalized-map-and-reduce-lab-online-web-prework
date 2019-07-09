1c4c07be29d9cd8a44a57772704e980f558cf06f
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-09 16:51:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,7 @@ def reduce(source_array, starting_point = 0)
       total = yield(total, source_array[i])
       i += 1 
   end 
-    if 
-      total = 0 
+    if total == 0 
       return true 
     end 
     return total  
